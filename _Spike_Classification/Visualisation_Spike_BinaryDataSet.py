cd794de5f288596190b1637bb28ef26f0df722fb
M.Adriana <59016422+mam619@users.noreply.github.com>
2020-08-28 12:28:41 +0100

    Changes in graphs for quantificaiton of SP for different years and months for report

--- _Spike_Classification/Visualisation_Spike_BinaryDataSet.py
+++ _Spike_Classification/Visualisation_Spike_BinaryDataSet.py
@@ -19,15 +19,15 @@ data = pd.read_csv('Spike_binary_1std.csv', index_col = 0)
 
 w = 55
 w_plot = 55
-fontsize = 13
+fontsize = 16
 
-plt.figure(figsize=(20,5))
+plt.figure(figsize=(15,4))
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['Offers'], label = 'Offer', linewidth = 2.5, color = 'steelblue')
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['sma'], label = 'Moving average', linestyle = 'dashed', color = 'black')
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['spike_upperlim'], color = 'black')
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['spike_lowerlim'], label = ' Spike limits', color = 'black')
 plt.fill_between(np.arange(0, (w_plot)),  data['spike_lowerlim'][-w_plot:],data['spike_upperlim'][-w_plot:], facecolor='skyblue', alpha=0.5)
-plt.title('Last accepted offer for the last day of 2018 with spike limits', fontsize = fontsize + 2)
+#plt.title('Last accepted offer for the last day of 2018 with spike limits', fontsize = fontsize + 2)
 plt.xticks(np.arange(0, (w_plot), 2), fontsize = fontsize)
 plt.yticks(fontsize = fontsize)
 plt.legend(fontsize = fontsize)
@@ -39,6 +39,7 @@ plt.ylim(50, 200)
 plt.minorticks_on()
 plt.grid(which='major', linestyle='-', linewidth='0.5')
 plt.grid(which='minor', linestyle=':', linewidth='0.5')
+plt.tight_layout()
 plt.show()
 #plt.savefig('Plot_Spike_limits_w_48.png')
 
@@ -52,15 +53,15 @@ data['spike_occurance'] = ((data['Offers'] > data['spike_upperlim']) | (data['Of
 
 w = 55
 w_plot = 55
-fontsize = 13
+fontsize = 16
 
-plt.figure(figsize=(20,5))
+plt.figure(figsize=(15, 4))
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['Offers'], label = 'Offer', linewidth = 2.5, color = 'steelblue')
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['sma'], label = 'Moving average', linestyle = 'dashed', color = 'black')
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['spike_upperlim'], color = 'black')
 plt.plot(np.arange(0, (w_plot)), data[-w_plot:]['spike_lowerlim'], label = ' Spike limits', color = 'black')
 plt.fill_between(np.arange(0, (w_plot)),  data['spike_lowerlim'][-w_plot:],data['spike_upperlim'][-w_plot:], facecolor='skyblue', alpha=0.5)
-plt.title('Last accepted offer for the last day of 2018 with spike limits', fontsize = fontsize + 2)
+#plt.title('Last accepted offer for the last day of 2018 with spike limits', fontsize = fontsize + 2)
 plt.xticks(np.arange(0, (w_plot), 2), fontsize = fontsize)
 plt.yticks(fontsize = fontsize)
 plt.legend(fontsize = fontsize)
@@ -72,6 +73,7 @@ plt.ylim(50, 200)
 plt.minorticks_on()
 plt.grid(which='major', linestyle='-', linewidth='0.5')
 plt.grid(which='minor', linestyle=':', linewidth='0.5')
+plt.tight_layout()
 plt.show()
 #plt.savefig('Plot_Spike_limits_w_8.png')
 
@@ -171,39 +173,40 @@ max_spike_16 = spike_var_16['num_spikes'].max()
 # spike occurences plot
 
 # 2018, 2017, 2016
-plt.figure(figsize=(15,5))
+plt.figure(figsize=(15,4.5))
 plt.plot(range_w, spike_var_18['num_spikes'], label = '2018', linewidth = 2, color = 'cornflowerblue')
 plt.plot(range_w, spike_var_17['num_spikes'], label = '2017', linewidth = 2, color = 'slateblue')
 plt.plot(range_w, spike_var_16['num_spikes'], label = '2016', linewidth = 2, color = 'plum')
 plt.ylabel('Number of SP', fontsize = fontsize)
-plt.xlabel('Window number (SP)', fontsize = fontsize)
+plt.xlabel('Rolling window number (SP)', fontsize = fontsize)
 plt.xticks(np.arange(0, 62, 2), fontsize = fontsize)
 plt.yticks(fontsize = fontsize)
 plt.scatter(spike_var_18[spike_var_18['num_spikes'] == max_spike_18].window, max_spike_18 , color = 'red', label = 'Maximum values')
-plt.text(spike_var_18[spike_var_18['num_spikes'] == max_spike_18].window , max_spike_18 + 200, '({},{})'.format(spike_var_18[spike_var_18['num_spikes'] == max_spike_18].window.iloc[0], max_spike_18))
+plt.text(spike_var_18[spike_var_18['num_spikes'] == max_spike_18].window , max_spike_18 + 200, '({},{})'.format(spike_var_18[spike_var_18['num_spikes'] == max_spike_18].window.iloc[0], max_spike_18), fontsize = fontsize - 2)
 plt.scatter(spike_var_17[spike_var_17['num_spikes'] == max_spike_17].window, max_spike_17 , color = 'red')
-plt.text(spike_var_17[spike_var_17['num_spikes'] == max_spike_17].window + 1, max_spike_17 - 200, '({},{})'.format(spike_var_17[spike_var_17['num_spikes'] == max_spike_17].window.iloc[0], max_spike_17))
+plt.text(spike_var_17[spike_var_17['num_spikes'] == max_spike_17].window + 0.5, max_spike_17 - 500, '({},{})'.format(spike_var_17[spike_var_17['num_spikes'] == max_spike_17].window.iloc[0], max_spike_17), fontsize = fontsize - 2)
 plt.scatter(spike_var_16[spike_var_16['num_spikes'] == max_spike_16].window, max_spike_16 , color = 'red')
-plt.text(spike_var_16[spike_var_16['num_spikes'] == max_spike_16].window , max_spike_16 + 200, '({},{})'.format(spike_var_16[spike_var_16['num_spikes'] == max_spike_16].window.iloc[0], max_spike_16))
-plt.title('Number of spike occurences for different roling windows', fontsize= fontsize + 2)
+plt.text(spike_var_16[spike_var_16['num_spikes'] == max_spike_16].window , max_spike_16 + 100, '({},{})'.format(spike_var_16[spike_var_16['num_spikes'] == max_spike_16].window.iloc[0], max_spike_16), fontsize = fontsize - 2)
+plt.title('Number of spike occurences using different roling windows\n', fontsize= fontsize + 2)
 plt.minorticks_on()
 plt.grid(which='major', linestyle='-', linewidth='0.5')
 plt.grid(which='minor', linestyle=':', linewidth='0.5')
-plt.legend(fontsize = fontsize)
+plt.legend(fontsize = fontsize - 3)
 plt.xlim(0, 60)
+plt.tight_layout()
 plt.show()
 #plt.savefig('Spike_occur_all_years.png')
 
 # plot of both spike occurences and normal opperation SP together
 # 2018
-plt.figure(figsize=(15,5))
-plt.plot(range_w, spike_var_18['num_spikes'], linewidth = 2, label = 'Number of SP with spike occurences', color = 'cornflowerblue')
-plt.plot(range_w, spike_var_18['num_normal'], linewidth = 2, label = 'Number of SP under normal operation', linestyle = 'dashed', color = 'cornflowerblue')
+plt.figure(figsize=(15,4.5))
+plt.plot(range_w, spike_var_18['num_spikes'], linewidth = 2, label = 'Spike occurence', color = 'cornflowerblue')
+plt.plot(range_w, spike_var_18['num_normal'], linewidth = 2, label = 'No spike occurence', linestyle = 'dashed', color = 'cornflowerblue')
 plt.ylabel('Number of SP', fontsize = fontsize)
 plt.xlabel('Window number (SP)', fontsize = fontsize)
-plt.title('Number of SP with spike occurences and normal regions for the year of 2018 for different rolling windows', fontsize= 15)
+plt.title('Number of SP with spike occurences and no spike occurence for 2018 using different rolling windows\n', fontsize = fontsize + 2)
 plt.xticks(np.arange(0, 62, 2), fontsize = fontsize)
-plt.yticks(fontsize = fontsize)
+plt.yticks([0, 2500, 5000, 7500, 10000, 12500, 15000, 17500], fontsize = fontsize)
 plt.xlim(0, 60)
 #plt.scatter(spike_var[spike_var['num_normal'] == max_num].window, max_num , color = 'red')
 plt.scatter(spike_var_18[spike_var_18['num_spikes'] == max_spike_18].window, max_spike_18 , color = 'red', label = 'Maximum value')
@@ -212,6 +215,7 @@ plt.minorticks_on()
 plt.grid(which='major', linestyle='-', linewidth='0.5')
 plt.grid(which='minor', linestyle=':', linewidth='0.5')
 plt.legend(fontsize = fontsize)
+plt.tight_layout()
 plt.show()
 #plt.savefig('Spike_occur_2018.png')
 
@@ -220,7 +224,6 @@ plt.show()
 # =============================================================================
 
 w = 48
-plt.figure(figsize=(15,5))
 data['sma'] = data['Offers'].rolling(window = w).mean()
 data['std'] = data['Offers'].rolling(w).std()
 data['spike_upperlim'] = data['sma'] + (data['std'])
@@ -268,8 +271,8 @@ for i in range(0, len(spike_occurance_18)):
     
 
 # PLOT RESULTS
-fig, ax1 = plt.subplots(figsize=(11,4))
-
+fig, ax1 = plt.subplots(figsize=(15,4.5))
+plt.rcParams.update({'font.size': 16})
 ax1.set_xlabel('Months of the year', fontsize = fontsize)
 ax1.set_ylabel('Number of SP', fontsize = fontsize)
 ax1.minorticks_on()
@@ -281,7 +284,7 @@ ax1.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], count_16, color = 'plum', labe
 ax1.set_xticks([1,2,3,4,5,6,7,8,9, 10, 11,12])
 ax1.set_xlim([1,12])
 ax1.legend(fontsize = fontsize)
-ax1.set_title('Number of spikes per month for 2018, 2017 and 2019 and respective percentages', fontsize = fontsize + 2)
+ax1.set_title('Number of spikes per month fand respective percentages\n', fontsize = fontsize + 3)
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 ax2.set_ylabel('Percentage of SP (%)', fontsize = fontsize)
@@ -289,7 +292,7 @@ ax2.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], perc_18, color = 'none')
 ax2.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], perc_17, color = 'none')
 ax2.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], perc_16, color = 'none')
 
-plt.rcParams.update({'font.size': 13})
+
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
