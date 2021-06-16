b97a3a29c8d191adef454c84742db8591ecc7c7d
Adriana Martins <am@arenko.group>
2021-06-16 09:24:52 +0100

    finish ML modellign

---
+++ random_forest.py
@@ -0,0 +1,45 @@
+# random forest (multi-var)
+
+import pandas as pd
+from datetime import datetime
+import pytz
+import json
+
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.ensemble import RandomForestRegressor
+
+import utils
+
+if __name__ == "__main__":
+
+    # import data
+    data = pd.read_csv("data/processed_data/data_final.csv", index_col=0, parse_dates=True)
+
+    # set prediction window according to the date range required
+    data = data.loc[data.index > datetime(2017, 6, 1, tzinfo=pytz.utc), :]
+
+    # Divide features and labels
+    y = data.pop("offers")
+    X = data
+
+    # create regressor
+    regressor = RandomForestRegressor(n_estimators=60)
+
+    # nested cross validation
+    tscv = TimeSeriesSplit(n_splits=6, max_train_size=365 * 48, test_size=48 * 30)
+
+    # perform nested cross validation and get results
+    y_test, y_pred = utils.my_cross_val_predict(regressor, X, y, tscv)
+
+    # calculate results
+    results = utils.get_results(y_test, y_pred)
+
+    # save results
+    with open("results/results_random_forest.json", "w") as f:
+        json.dump(results, f)
+
+    utils.plot_results(
+        y_test, y_pred, filename="random_forest", window_plot=200, fontsize=14, fig_size=(15, 5)
+    )
+
+    utils.plot_scatter(y_test, y_pred, filename="random_forest")
